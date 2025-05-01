6ac7730345ebd69e60563aed19e993f197cfbaf7
Roland Sherwin <RolandSherwin@protonmail.com>
2025-05-01 15:29:53 +0530

    feat(reachability_check): return an error when we don't have enough contacts

--- ant-networking/src/error.rs
+++ ant-networking/src/error.rs
@@ -33,6 +33,8 @@ pub enum ReachabilityCheckError {
     LocalAdapterShouldNotBeEmpty,
     #[error("External address not found")]
     ExternalAddrsShouldNotBeEmpty,
+    #[error("No more contacts to dial")]
+    NoMoreContacts,
 }
 
 /// Network Errors
